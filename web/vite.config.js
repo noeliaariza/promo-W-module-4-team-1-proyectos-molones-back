0fa0bb8bbaf315820edf33ab84712da92039abf5
Paula <psanzilundain@gmail.com>
2024-04-08 13:03:08 +0200

    removed base line in vite.config

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: { watch: { usePolling: true } },
-  base: "victoriagz-project-promo-W-module-3-team-1",
 });
