accd7d852c8e7e7af4cc956de71c2c1d6975a48c
Paula <psanzilundain@gmail.com>
2024-04-10 20:45:12 +0200

    style

--- web/src/components/Form.jsx
+++ web/src/components/Form.jsx
@@ -1,7 +1,7 @@
-import PropTypes from "prop-types";
-import Button from "./Button";
-import GetAvatar from "./GetAvatar";
-import CardUrl from "./CardUrl";
+import PropTypes from 'prop-types';
+import Button from './Button';
+import GetAvatar from './GetAvatar';
+import CardUrl from './CardUrl';
 
 function Form({ onChangeValue, onClickSave, cardUrl, infoProject }) {
   const handleInfoProject = (event, id) => {
@@ -15,113 +15,113 @@ function Form({ onChangeValue, onClickSave, cardUrl, infoProject }) {
 
   return (
     <form
-      className="addForm"
+      className='addForm'
       onSubmit={(e) => {
         e.preventDefault();
         onClickSave();
       }}
     >
-      <h2 className="title">Información</h2>
-      <fieldset className="addForm__group">
-        <legend className="addForm__title">Cuéntanos sobre el proyecto</legend>
-        <p className="title2"> Los campos marcados con * son obligatorios </p>
+      <h2 className='title'>Información</h2>
+      <fieldset className='addForm__group'>
+        <legend className='addForm__title'>Cuéntanos sobre el proyecto</legend>
+        <p className='title2'> Los campos marcados con * son obligatorios </p>
         <input
-          className="addForm__input"
-          type="text"
-          name="name"
-          id="name"
+          className='addForm__input'
+          type='text'
+          name='name'
+          id='name'
           value={infoProject.name}
-          placeholder="Nombre del proyecto"
+          placeholder='Nombre del proyecto *'
           onChange={handleInfoProject}
         />
         <input
-          className="addForm__input"
-          type="text"
-          name="slogan"
-          id="slogan"
+          className='addForm__input'
+          type='text'
+          name='slogan'
+          id='slogan'
           value={infoProject.slogan}
-          placeholder="Slogan"
+          placeholder='Slogan *'
           onChange={handleInfoProject}
         />
-        <div className="addForm__2col">
+        <div className='addForm__2col'>
           <input
-            className="addForm__input"
-            type="url"
-            name="repo"
-            id="repo"
+            className='addForm__input'
+            type='url'
+            name='repo'
+            id='repo'
             value={infoProject.repo}
-            placeholder="Repositorio"
+            placeholder='Repositorio *'
             onChange={handleInfoProject}
           />
           <input
-            className="addForm__input"
-            type="url"
-            name="demo"
-            id="demo"
+            className='addForm__input'
+            type='url'
+            name='demo'
+            id='demo'
             value={infoProject.demo}
-            placeholder="Demo"
+            placeholder='Demo *'
             onChange={handleInfoProject}
           />
         </div>
         <input
-          className="addForm__input"
-          type="text"
-          name="technologies"
-          id="technologies"
+          className='addForm__input'
+          type='text'
+          name='technologies'
+          id='technologies'
           value={infoProject.technologies}
-          placeholder="Tecnologías"
+          placeholder='Tecnologías *'
           onChange={handleInfoProject}
         />
 
         <textarea
-          className="addForm__input"
-          type="text"
-          name="desc"
-          id="desc"
-          placeholder="Descripción"
+          className='addForm__input'
+          type='text'
+          name='desc'
+          id='desc'
+          placeholder='Descripción *'
           value={infoProject.desc}
-          rows="5"
+          rows='5'
           onChange={handleInfoProject}
         ></textarea>
       </fieldset>
 
-      <fieldset className="addForm__group">
-        <legend className="addForm__title">Cuéntanos sobre la autora</legend>
+      <fieldset className='addForm__group'>
+        <legend className='addForm__title'>Cuéntanos sobre la autora</legend>
         <input
-          className="addForm__input"
-          type="text"
-          name="autor"
-          id="autor"
+          className='addForm__input'
+          type='text'
+          name='autor'
+          id='autor'
           value={infoProject.autor}
-          placeholder="Nombre"
+          placeholder='Nombre *'
           onChange={handleInfoProject}
         />
         <input
-          className="addForm__input"
-          type="text"
-          name="job"
-          id="job"
+          className='addForm__input'
+          type='text'
+          name='job'
+          id='job'
           value={infoProject.job}
-          placeholder="Trabajo"
+          placeholder='Trabajo *'
           onChange={handleInfoProject}
         />
       </fieldset>
 
-      <fieldset className="addForm__group--upload">
+      <fieldset className='addForm__group--upload'>
         <GetAvatar
           updateImage={(ev) => {
-            handleInfoProject(ev, "image");
+            handleInfoProject(ev, 'image');
           }}
-          text="Subir foto del proyecto"
+          text='Subir foto del proyecto'
         />
         <GetAvatar
           updateImage={(ev) => {
-            handleInfoProject(ev, "photo");
+            handleInfoProject(ev, 'photo');
           }}
-          text="Subir foto de la autora"
+          text='Subir foto de la autora'
         />
 
-        <Button type="submit" textContent="Guardar proyectos" />
+        <Button type='submit' textContent='Guardar proyectos' />
       </fieldset>
       {cardUrl && <CardUrl cardUrl={cardUrl} />}
     </form>
